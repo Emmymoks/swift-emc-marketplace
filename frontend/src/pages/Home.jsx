c1579a1e55c1a6d9627c59fe024d1e4ed0427820
Emmymoks <mokwunyeemmanuel@gmail.com>
2025-10-04 09:15:41 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Home(){
   return (
-    <div>
+    <div className="page">
       <h2>Welcome to Swift EMC Marketplace</h2>
       <p>Buy and sell worldwide. Modern, fast, and secure.</p>
     </div>
